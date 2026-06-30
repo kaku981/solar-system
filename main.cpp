d538d790cf146bc969da92daf42039b38ac234a2
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-06-30 09:43:05 +0800

    Created a window with opengl and glfw

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
-#include <iostream>
+#include <GLFW/glfw3.h>
 
-int main() {
-    std::cout << "hello, world!";
+int main(void)
+{
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
