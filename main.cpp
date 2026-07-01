4240123f3b9fb2cd6a674bafb71a7048a1bf54f2
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-07-02 07:59:55 +0800

    Drawing two points and also vertex

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 #include <GL/glut.h>
 
 void display();
+void reshape(int, int);
 
 void init() {
-     glClearColor(1.0, 1.0, 0.0, 1.0);
+     glClearColor(0.0, 0.0, 0.0, 1.0);
      
 }
 
@@ -19,6 +20,7 @@ int main(int argc, char**argv) {
     glutCreateWindow("Window 1");
 
     glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
     init();
 
     glutMainLoop();
@@ -29,7 +31,20 @@ void display() {
      glClear(GL_COLOR_BUFFER_BIT);
      glLoadIdentity();
 
-     //draw
+     glPointSize(10.0);
+     glBegin(GL_POINTS);
+
+     glVertex2f(5, 5);
+     glVertex2f(-5, -5);
+     glEnd();
 
      glFlush();
+}
+
+void reshape(int w, int h) {
+     glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+     glMatrixMode(GL_PROJECTION);
+     glLoadIdentity();
+     gluOrtho2D(-10, 10, -10, 10);
+     glMatrixMode(GL_MODELVIEW);
 }
