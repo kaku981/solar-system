5b14a3f0e240a3a4ab449d1e426ee54df6833cd0
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-07-01 16:23:36 +0800

    Completed display function and turn window color to yellow

--- main.cpp
+++ main.cpp
@@ -4,19 +4,32 @@
 
 void display();
 
+void init() {
+     glClearColor(1.0, 1.0, 0.0, 1.0);
+     
+}
+
 int main(int argc, char**argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB);
 
     glutInitWindowPosition(200, 100);
     glutInitWindowSize(500, 500);
-  
+
     glutCreateWindow("Window 1");
 
     glutDisplayFunc(display);
+    init();
 
     glutMainLoop();
 
 }
 
-void display() {}
+void display() {
+     glClear(GL_COLOR_BUFFER_BIT);
+     glLoadIdentity();
+
+     //draw
+
+     glFlush();
+}
