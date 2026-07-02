98f7c3857709317a85c4e84bc24d10b5fc3447d2
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-07-02 08:34:08 +0800

    Glut animation

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 void display();
 void reshape(int, int);
+void timer(int);
 
 void init() {
      glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -12,7 +13,7 @@ void init() {
 
 int main(int argc, char**argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 
     glutInitWindowPosition(200, 100);
     glutInitWindowSize(500, 500);
@@ -21,11 +22,15 @@ int main(int argc, char**argv) {
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
+    glutTimerFunc(1000, timer, 0);
     init();
 
     glutMainLoop();
 
 }
+
+float x_position = -10.0;
+int state = 1;
 
 void display() {
      glClear(GL_COLOR_BUFFER_BIT);
@@ -33,14 +38,14 @@ void display() {
 
      glBegin(GL_POLYGON);
 
-     glVertex2f(0.0, 5.0);
-     glVertex2f(4.0, -3.0);
-     glVertex2f(-4.0, -3.0);
-     glVertex2f(5.0, -2.0);
+     glVertex2f(x_position, 1.0);
+     glVertex2f(x_position, -1.0);
+     glVertex2f(x_position+2.0, -1.0);
+     glVertex2f(x_position+2.0, 1.0);
 
      glEnd();
 
-     glFlush();
+     glutSwapBuffers();
 }
 
 void reshape(int w, int h) {
@@ -49,4 +54,24 @@ void reshape(int w, int h) {
      glLoadIdentity();
      gluOrtho2D(-10, 10, -10, 10);
      glMatrixMode(GL_MODELVIEW);
+}
+
+void timer(int) {
+     glutPostRedisplay();
+     glutTimerFunc(1000/60, timer, 0);
+
+     switch(state) {
+     case 1:
+         if(x_position < 8)
+             x_position += 0.15;
+         else
+             state = -1;
+         break;
+     case -1:
+         if(x_position >- 10)
+             x_position -= 0.15;
+         else
+             state = 1;
+         break;
+     }
 }
