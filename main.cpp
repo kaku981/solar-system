8dcc3c5888dd3c0541d16815bc10d9a86bd57290
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-07-06 17:11:27 +0800

    Draw a circle

--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
 #include <GL/glut.h>
+#include <math.h>
 
-void display();
+void drawCircle(float cx, float cy, float r, int num_segments) {
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2f(cx, cy);
+    for(int i = 0; i <= num_segments; i++) {
+        float theta = 2.0f * 3.1415926f * float(i) / float(num_segments);
+        float x = r * cosf(theta) + cx;
+        float y = r * sinf(theta) + cy;
+        glVertex2f(x, y);
+    }
+    glEnd();
+}
+
+void display() {
+     glClear(GL_COLOR_BUFFER_BIT);
+     glColor3f(1.0, 1.0, 1.0);
+     drawCircle(0.5, 0.5, 0.2, 50);
+
+     glFlush();
+}
 
 void init() {
-    glClearColor(1.0, 1.0, 0.0, 1.0);
+     glClearColor(0.0, 0.0, 0.0, 1.0);
+     glMatrixMode(GL_PROJECTION);
+     glLoadIdentity();
+     glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 }
 
 int main(int argc, char**argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 
     glutInitWindowPosition(200, 100);
     glutInitWindowSize(500, 500);
@@ -19,13 +41,5 @@ int main(int argc, char**argv) {
     init();
 
     glutMainLoop();
-}
-
-void display() {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glLoadIdentity();
-
-    //draw
-
-    glFlush();
+    return 0;
 }
