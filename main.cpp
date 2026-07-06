181ea7444d876a18351c43118b7d84188ccdf34c
kaku981 <153264639+kaku981@users.noreply.github.com>
2026-07-06 16:21:37 +0800

    Created a window and change its color to yellow

--- main.cpp
+++ main.cpp
@@ -1,47 +1,31 @@
 #include <GL/glut.h>
-#include <math.h>
 
 void display();
 
-void drawCircle(float radius, int segments) {
-    glBegin(GL_TRIANGLE_FAN);
-    glVertex2f(0, 0);
-
-    for (int i = 0; i <= segments; i++) {
-         float theta = 2.0f * 3.1415926f * float(i) / float(segments);
-         float x = radius * cos(theta);
-         float y = radius * sin(theta);
-         glVertex2f(x, y);
-    }
-    glEnd();
+void init() {
+    glClearColor(1.0, 1.0, 0.0, 1.0);
 }
 
 int main(int argc, char**argv) {
     glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB);
 
-    glutInitWindowPosition(100, 100);
-    glutInitWindowSize(800, 800);
+    glutInitWindowPosition(200, 100);
+    glutInitWindowSize(500, 500);
 
     glutCreateWindow("solar system");
 
     glutDisplayFunc(display);
-
-    glOrtho(0, 1, 0, 1, -1, 1);
+    init();
 
     glutMainLoop();
-
-    return 0;
 }
 
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(1.0f, 0.0f, 0.0f);
     glLoadIdentity();
 
-    glTranslatef(0.5f, 0.5f, 0.0f);
+    //draw
 
-    glScalef(0.3f, 0.3f, 1.0f);
-
-    drawCircle(1.0f, 100);
     glFlush();
 }
